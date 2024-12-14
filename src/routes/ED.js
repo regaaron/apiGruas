933e04bb66a6fb46059b63d4309b367f772af383
CocoAsombroso <Ivanelmen420@gmail.com>
2024-12-14 10:38:47 -0600

    ups
    asdfasdfasd

--- src/routes/ED.js
+++ src/routes/ED.js
@@ -7,8 +7,8 @@ const router = express.Router();
 
 // Ruta para encriptar `clientes.json`
 router.post('/encrypt', async (req, res) => {
-  const inputFileName = 'conductores.json';
-  const outputFileName = 'conductores.encrypted';
+  const inputFileName = 'clientes.json';
+  const outputFileName = 'clientes.encrypted';
 
   try {
     const message = await encryptFile(`../jsonFiles/${inputFileName}`, `../encripFiles/${outputFileName}`);
